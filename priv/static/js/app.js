16c6e8fd7edfe617f026615297e9fb98e7ac9a16
Krzysztof Wende <iraasta@gmail.com>
2015-09-14 14:29:48 +0200

    map

--- priv/static/js/app.js
+++ priv/static/js/app.js
@@ -1147,7 +1147,7 @@ var C_WIDTH = heatmapCanvas.offsetWidth;
 var C_HEIGHT = heatmapCanvas.offsetHeight;
 var MAX_VAL = 350;
 var CLEAR_FACTOR = 0.98;
-var DP_FORCE = 0.01;
+var DP_FORCE = 0.001;
 
 chan.on("datapoint", function (dp) {
   addData(dp.x * 10, dp.y * 10, 647 - dp.distance, DP_FORCE);
